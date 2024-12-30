2f3874618560fc10d4fab059085973fa436e884f
redblackblossom <physicsdev1029@gmail.com>
2024-12-30 12:30:59 +0900

    [FEAT] 프로젝트 시간대 설정

--- src/main/java/com/brainpix/BrainpixApplication.java
+++ src/main/java/com/brainpix/BrainpixApplication.java
@@ -1,10 +1,19 @@
 package com.brainpix;
 
+import jakarta.annotation.PostConstruct;
+
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BrainpixApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrainpixApplication.class, args);
