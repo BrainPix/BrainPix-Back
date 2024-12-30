b2d913b1294757358483cdb28a63586840e97f25
redblackblossom <physicsdev1029@gmail.com>
2024-12-30 11:38:46 +0900

    [FEAT] 프로젝트 생성

---
+++ src/main/java/com/brainpix/BrainpixApplication.java
@@ -0,0 +1,13 @@
+package com.brainpix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainpixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainpixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brainpix/BrainpixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainpix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainpixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
