546cd7e375ffe2f65593d1623fea232e9e463119
redblackblossom <physicsdev1029@gmail.com>
2025-01-14 21:42:16 +0900

    [FIX] 모집 분야 인스턴스 협업 광장에서 요청 과제로 옮김

--- src/main/java/com/brainpix/post/entity/collaboration_hub/CollaborationHub.java
+++ src/main/java/com/brainpix/post/entity/collaboration_hub/CollaborationHub.java
@@ -7,8 +7,6 @@ import com.brainpix.post.entity.BasePost;
 import com.brainpix.user.entity.User;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,16 +25,12 @@ public class CollaborationHub extends BasePost {
 	private LocalDateTime deadline;
 	private String link;
 
-	@Enumerated(EnumType.STRING)
-	private CollaborationType collaborationType;
-
 	@Builder
 	public CollaborationHub(User writer, String title, String contest, String category, Boolean openMyProfile,
 		Long viewCount, List<String> imageList, List<String> attachmentFileList, LocalDateTime deadline,
-		CollaborationType collaborationType, String link) {
+		String link) {
 		super(writer, title, contest, category, openMyProfile, viewCount, imageList, attachmentFileList);
 		this.deadline = deadline;
-		this.collaborationType = collaborationType;
 		this.link = link;
 	}
 }
--- src/main/java/com/brainpix/post/entity/request_task/RequestTask.java
+++ src/main/java/com/brainpix/post/entity/request_task/RequestTask.java
@@ -4,9 +4,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.brainpix.post.entity.BasePost;
+import com.brainpix.post.entity.collaboration_hub.CollaborationType;
 import com.brainpix.user.entity.User;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,10 +27,15 @@ public class RequestTask extends BasePost {
 
 	private LocalDateTime deadline;
 
+	@Enumerated(EnumType.STRING)
+	private CollaborationType collaborationType;
+
 	@Builder
 	public RequestTask(User writer, String title, String contest, String category, Boolean openMyProfile,
-		Long viewCount, List<String> imageList, List<String> attachmentFileList, LocalDateTime deadline) {
+		Long viewCount, List<String> imageList, List<String> attachmentFileList, LocalDateTime deadline,
+		CollaborationType collaborationType) {
 		super(writer, title, contest, category, openMyProfile, viewCount, imageList, attachmentFileList);
 		this.deadline = deadline;
+		this.collaborationType = collaborationType;
 	}
 }
