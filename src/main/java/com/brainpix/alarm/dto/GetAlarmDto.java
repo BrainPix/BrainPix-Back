5390217582570e36a99b00f640d0ded0247ce7b4
qmean <qmean59@gmail.com>
2025-01-14 23:21:44 +0900

    [FEAT] 알림 관련 api request 및 response에 사용할 dto와 converter 설정

---
+++ src/main/java/com/brainpix/alarm/dto/GetAlarmDto.java
@@ -0,0 +1,38 @@
+package com.brainpix.alarm.dto;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * 읽지 않은 알람을 가져오는 API 의 응답 DTO
+ */
+public class GetAlarmDto {
+
+	/**
+	 * 읽지 않은 알람을 가져오는 API 의 응답
+	 */
+	@Getter
+	@Builder
+	public static class Response {
+		private Integer totalPage;
+		private Long totalElements;
+		private Integer listSize;
+		private Boolean isLast;
+		private List<AlarmDetail> alarmDetailList;
+	}
+
+	/**
+	 * 알람 상세 정보
+	 */
+	@Getter
+	@Builder
+	public static class AlarmDetail {
+		private String alarmId;
+		private Boolean isRead;
+		private String header;
+		private String message;
+		private String redirectUrl;
+	}
+}
