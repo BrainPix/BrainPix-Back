111e5e3fc88eebd87269161afa0480c543ed0b49
qmean <qmean59@gmail.com>
2025-01-15 18:53:50 +0900

    [FEAT] 알림 삭제 및 휴지통 보내기 로직을 위한 repository 메서드 추가

--- src/main/java/com/brainpix/alarm/repository/AlarmRepository.java
+++ src/main/java/com/brainpix/alarm/repository/AlarmRepository.java
@@ -1,5 +1,7 @@
 package com.brainpix.alarm.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -8,6 +10,8 @@ import com.brainpix.alarm.model.Alarm;
 
 public interface AlarmRepository extends MongoRepository<Alarm, String> {
 	Page<Alarm> findByReceiverIdAndTrashed(Long receiverId, boolean trashed, Pageable pageable);
+
+	List<Alarm> findAllByReceiverIdAndTrashed(Long receiverId, boolean trashed);
 
 	Long countAlarmByReceiverIdAndIsRead(Long receiverId, Boolean isRead);
 }
