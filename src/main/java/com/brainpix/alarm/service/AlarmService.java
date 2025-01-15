c29027cc7053be32e1d82368f0bed51d65793b26
qmean <qmean59@gmail.com>
2025-01-15 20:39:18 +0900

    [FEAT] 알림을 휴지통 관련 로직 추가 및 정렬 기준 수정

--- src/main/java/com/brainpix/alarm/service/AlarmService.java
+++ src/main/java/com/brainpix/alarm/service/AlarmService.java
@@ -1,5 +1,8 @@
 package com.brainpix.alarm.service;
 
+import java.util.List;
+import java.util.Objects;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -29,9 +32,8 @@ public class AlarmService {
 	 * @param alarmId 알림 ID
 	 * @throws BrainPixException 알림이 이미 읽음 처리된 경우
 	 */
-	public void readAlarm(String alarmId) {
-		Alarm alarm = alarmRepository.findById(alarmId)
-			.orElseThrow(() -> new BrainPixException(AlarmErrorCode.ALARM_NOT_FOUND));
+	public void readAlarm(String alarmId, Long userId) {
+		Alarm alarm = getAlarm(alarmId, userId);
 
 		if (alarm.getIsRead()) {
 			throw new BrainPixException(AlarmErrorCode.ALARM_ALREADY_READ);
@@ -48,8 +50,7 @@ public class AlarmService {
 	 */
 	public GetAlarmDto.Response getAlarm(Pageable pageable, Long receiverId) {
 		// sort 추가하여 isRead = false 먼저 보여줌
-		PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
-			Sort.by(Sort.Order.asc("isRead"), Sort.Order.desc("createdAt")));
+		PageRequest pageRequest = getPageRequestOrderByCreatedAt(pageable);
 
 		Page<Alarm> alarmPage = alarmRepository.findByReceiverIdAndTrashed(receiverId, false, pageRequest);
 
@@ -63,8 +64,7 @@ public class AlarmService {
 	 * @return GetAlarmDto.Response
 	 */
 	public GetAlarmDto.Response getTrashAlarm(Pageable pageable, Long receiverId) {
-		PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
-			Sort.by(Sort.Order.desc("createdAt")));
+		PageRequest pageRequest = getPageRequestOrderByCreatedAt(pageable);
 
 		Page<Alarm> alarmPage = alarmRepository.findByReceiverIdAndTrashed(receiverId, true, pageRequest);
 
@@ -95,5 +95,92 @@ public class AlarmService {
 		Long alarmCount = alarmRepository.countAlarmByReceiverIdAndIsRead(userId, false);
 
 		return GetUnreadAlarmDto.Response.builder().alarmCount(alarmCount).build();
+	}
+
+	/**
+	 * 알림을 휴지통으로 보냅니다
+	 * @param alarmId 알림 ID
+	 * @param userId 사용자 ID
+	 */
+	public void addTrashAlarm(String alarmId, Long userId) {
+		Alarm alarm = getAlarm(alarmId, userId);
+
+		if (alarm.getTrashed()) {
+			throw new BrainPixException(AlarmErrorCode.ALARM_ALREADY_TRASHED);
+		}
+
+		alarm.addTrash();
+
+		alarmRepository.save(alarm);
+	}
+
+	/**
+	 * 휴지통에서 알림을 복원합니다
+	 * @param alarmId 알림 ID
+	 * @param userId 사용자 ID
+	 */
+	public void restoreAlarm(String alarmId, Long userId) {
+		Alarm alarm = getAlarm(alarmId, userId);
+
+		if (!alarm.getTrashed()) {
+			throw new BrainPixException(AlarmErrorCode.ALARM_NOT_TRASHED);
+		}
+
+		alarm.restore();
+
+		alarmRepository.save(alarm);
+	}
+
+	/**
+	 * 하나의 알림을 삭제합니다
+	 * @param alarmId 알림 ID
+	 * @param userId 사용자 ID
+	 */
+	public void deleteOneAlarm(String alarmId, Long userId) {
+		Alarm alarm = getAlarm(alarmId, userId);
+
+		if (!alarm.getTrashed()) {
+			throw new BrainPixException(AlarmErrorCode.ALARM_NOT_TRASHED);
+		}
+
+		alarmRepository.delete(alarm);
+	}
+
+	/**
+	 * 휴지통에 있는 모든 알림을 삭제합니다
+	 * @param userId 사용자 ID
+	 */
+	public void deleteAllAlarm(Long userId) {
+		List<Alarm> trashedAlarm = alarmRepository.findAllByReceiverIdAndTrashed(userId, true);
+
+		alarmRepository.deleteAll(trashedAlarm);
+	}
+
+	/**
+	 * 알림 ID로 알림을 조회합니다
+	 * @param alarmId 알림 ID
+	 * @param userId 사용자 ID
+	 * @throws BrainPixException 알림이 존재하지 않거나 알림 수신자가 아닌 경우
+	 * @return Alarm
+	 */
+	private Alarm getAlarm(String alarmId, Long userId) {
+		Alarm alarm = alarmRepository.findById(alarmId)
+			.orElseThrow(() -> new BrainPixException(AlarmErrorCode.ALARM_NOT_FOUND));
+
+		if (!Objects.equals(alarm.getReceiverId(), userId)) {
+			throw new BrainPixException(AlarmErrorCode.NOT_ALARM_RECEIVER);
+		}
+
+		return alarm;
+	}
+
+	/**
+	 * 생성일자를 기준으로 내림차순 정렬이 추가된 PageRequest를 반환합니다
+	 * @param pageable 페이징 정보
+	 * @return PageRequest
+	 */
+	private PageRequest getPageRequestOrderByCreatedAt(Pageable pageable) {
+		return PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
+			Sort.by(Sort.Order.desc("createdAt")));
 	}
 }
