df241bf53aa6e95e74288ef42b5cae9e3a1eb742
qmean <qmean59@gmail.com>
2025-01-15 20:37:47 +0900

    [FEAT] 알림중 휴지통 관련 에러처리를 위한 ErrorCode 추가

--- src/main/java/com/brainpix/api/code/error/AlarmErrorCode.java
+++ src/main/java/com/brainpix/api/code/error/AlarmErrorCode.java
@@ -11,7 +11,11 @@ public enum AlarmErrorCode implements ErrorCode {
 	ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "ALARM404", "알림을 찾을 수 없습니다."),
 
 	ALARM_ALREADY_READ(HttpStatus.BAD_REQUEST, "ALARM400", "이미 읽은 알림입니다."),
-	;
+	NOT_ALARM_RECEIVER(HttpStatus.BAD_REQUEST, "ALARM400", "알림 수신자가 아닙니다."),
+
+	ALARM_ALREADY_TRASHED(HttpStatus.BAD_REQUEST, "ALARM400", "이미 휴지통에 있는 알림입니다."),
+	ALARM_NOT_TRASHED(HttpStatus.BAD_REQUEST, "ALARM400", "휴지통에 없는 알림입니다.");
+	
 	private final HttpStatus httpStatus;
 	private final String code;
 	private final String message;
