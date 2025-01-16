97b175f0856c6e5d531fcb52d506291d55880423
redblackblossom <physicsdev1029@gmail.com>
2025-01-16 10:49:47 +0900

    [FEAT] BasePost를 MappedSupperClass가 아닌 상속 전략으로 이용

--- src/main/java/com/brainpix/post/entity/request_task/RequestTask.java
+++ src/main/java/com/brainpix/post/entity/request_task/RequestTask.java
@@ -3,7 +3,7 @@ package com.brainpix.post.entity.request_task;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.brainpix.post.entity.BasePost;
+import com.brainpix.post.entity.Post;
 import com.brainpix.post.entity.IdeaMarketAuth;
 import com.brainpix.post.entity.collaboration_hub.CollaborationType;
 import com.brainpix.user.entity.User;
@@ -11,9 +11,6 @@ import com.brainpix.user.entity.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,11 +18,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @Getter
-public class RequestTask extends BasePost {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
+public class RequestTask extends Post {
 	private LocalDateTime deadline;
 
 	@Enumerated(EnumType.STRING)
