952ff71bcc81d913346dadfc149eac066206704e
pywoo <kfdsy0103@naver.com>
2025-01-19 20:27:50 +0900

    [FIX] postId로 댓글 목록 조회 시 쿼리 수정 및 주석 추가

--- src/main/java/com/brainpix/post/repository/CommentCustomRepositoryImpl.java
+++ src/main/java/com/brainpix/post/repository/CommentCustomRepositoryImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import com.brainpix.post.entity.Comment;
 import com.brainpix.post.entity.QComment;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -25,18 +26,26 @@ public class CommentCustomRepositoryImpl implements CommentCustomRepository{
 	@Override
 	public Page<Comment> findByParentPostId(Long parentPostId, Pageable pageable) {
 
-		BooleanBuilder builder = new BooleanBuilder();
-
-		// 조회 쿼리 (Comment 테이블끼리 조인한뒤, 1. 부모 정렬, 2. 같은 그룹 내에서 자식끼리 정렬)
 		List<Comment> result = queryFactory
 			.selectFrom(comment)
-			.leftJoin(comment.parentComment).on(comment.parentComment.eq(comment))
+			.leftJoin(comment.parentComment)	// comment 끼리 조인
 			.where(
 				comment.parentPost.id.eq(parentPostId)
 			)
 			.orderBy(
-				comment.createdAt.asc(),
-				comment.parentComment.createdAt.asc()
+				 // 1. 부모는 자신의 ID를 기준, 자식은 부모 댓글의 ID를 기준으로 정렬.
+				 // 결과적으로 부모 ID로 부모와 자식 그룹화
+				Expressions.stringTemplate(
+					"function('COALESCE', {0}, {1})",
+					comment.parentComment.id, comment.id
+				).asc(),
+				// 2. 그룹화 된 후, 같은 그룹 내에서 부모와 자식 분리 (부모가 우선 나오도록)
+				Expressions.stringTemplate(
+					"case when {0} is null then 0 else 1 end",
+					comment.parentComment.id
+				).asc(),
+				// 3. 자식 내에서 오래된 순으로 정렬
+				comment.createdAt.asc()
 			)
 			.offset(pageable.getOffset())
 			.limit(pageable.getPageSize())
