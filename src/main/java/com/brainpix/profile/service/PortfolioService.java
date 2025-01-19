95a239718a79ec9eab6e581aeafb3121258b7159
Dongjun <ldj990517@gmail.com>
2025-01-19 22:44:19 +0900

    [REFACT] 불필요한 @Transactional 수정했습니다 #29

--- src/main/java/com/brainpix/profile/service/PortfolioService.java
+++ src/main/java/com/brainpix/profile/service/PortfolioService.java
@@ -1,6 +1,12 @@
 package com.brainpix.profile.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.brainpix.profile.dto.request.PortfolioRequest;
+import com.brainpix.profile.dto.request.SpecializationRequest;
 import com.brainpix.profile.dto.response.PortfolioDetailResponse;
 import com.brainpix.profile.dto.response.PortfolioResponse;
 import com.brainpix.profile.entity.Portfolio;
@@ -9,86 +15,97 @@ import com.brainpix.profile.repository.PortfolioRepository;
 import com.brainpix.profile.repository.ProfileRepository;
 import com.brainpix.profile.repository.UserRepository;
 import com.brainpix.user.entity.User;
-import java.util.List;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PortfolioService {
 
-    private final UserRepository userRepository;
-    private final PortfolioRepository portfolioRepository;
-    private final ProfileRepository profileRepository;
+	private final UserRepository userRepository;
+	private final PortfolioRepository portfolioRepository;
+	private final ProfileRepository profileRepository;
 
+	@Transactional
+	public Long createPortfolio(long userId, PortfolioRequest request) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new IllegalArgumentException(
+				"%d는 존재하지 않는 userId".formatted(userId)
+			));
 
-    @Transactional
-    public Long createPortfolio(long userId, PortfolioRequest request){
-        User user=userRepository.findById(userId)
-            .orElseThrow(()->new IllegalArgumentException(
-                "%d는 존재하지 않는 userId".formatted(userId)));
+		Profile profile = profileRepository.findById(user.getProfileId())
+			.orElseThrow();
 
-        Profile profile= profileRepository.findById(user.getProfileId())
-            .orElseThrow();
-        Portfolio portfolio=request.toEntity(profile);
+		Portfolio portfolio = Portfolio.create(
+			profile,
+			request.title(),
+			request.specializations().stream()
+				.map(SpecializationRequest::toDomain)
+				.toList(),
+			request.startDate(),
+			request.endDate(),
+			request.content()
+		);
 
-        portfolioRepository.save(portfolio);
+		portfolioRepository.save(portfolio);
 
-        return portfolio.getId();
-    }
+		return portfolio.getId();
+	}
 
-    @Transactional
-    public void updatePortfolio(long userId,long portfolioId, PortfolioRequest request){
+	@Transactional
+	public void updatePortfolio(long userId, long portfolioId, PortfolioRequest request) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new IllegalArgumentException(
+				"%d는 존재하지 않는 userId".formatted(userId)
+			));
 
-        User user=userRepository.findById(userId)
-            .orElseThrow(()->new IllegalArgumentException(
-                "%d는 존재하지 않는 userId".formatted(userId)
-            ));
+		Portfolio portfolio = portfolioRepository.findById(portfolioId)
+			.orElseThrow(() -> new IllegalArgumentException(
+				"%d는 존재하지 않는 portfolioId".formatted(portfolioId)
+			));
 
-        Portfolio portfolio=portfolioRepository.findById(portfolioId)
-            .orElseThrow(()->new IllegalArgumentException(
-                "%d는 존재하지 않는 portfolioId".formatted(portfolioId)
-            ));
+		if (!portfolio.getProfile().getId().equals(user.getProfileId())) {
+			throw new IllegalArgumentException("본인 소유의 포트폴리오가 아닙니다");
+		}
 
-        if (portfolio.getProfile().getId()!=user.getProfileId()){
-            throw new IllegalArgumentException("본인 소유의 포트폴리오가 아닙니다");
-        }
+		portfolio.update(
+			request.title(),
+			request.specializations().stream()
+				.map(SpecializationRequest::toDomain)
+				.toList(),
+			request.startDate(),
+			request.endDate(),
+			request.content()
+		);
+	}
 
-        request.applyTo(portfolio);
-    }
+	public Page<PortfolioResponse> findAllMyPortfolios(long userId, Pageable pageable) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new IllegalArgumentException(
+				"%d는 존재하지 않는 userId".formatted(userId)));
+		Profile profile = profileRepository.findById(user.getProfileId())
+			.orElseThrow();
 
-    @Transactional
-    public Page<PortfolioResponse> findAllMyPortfolios(long userId, Pageable pageable){
-        User user = userRepository.findById(userId)
-            .orElseThrow(() -> new IllegalArgumentException(
-                "%d는 존재하지 않는 userId".formatted(userId)));
-        Profile profile=profileRepository.findById(user.getProfileId())
-            .orElseThrow();
+		Page<Portfolio> portfolio = portfolioRepository.findByProfile(profile, pageable);
 
-        Page<Portfolio> portfolio = portfolioRepository.findByProfile(profile,pageable);
+		return portfolio.map(PortfolioResponse::from);
 
-        return portfolio.map(PortfolioResponse::from);
+	}
 
-    }
+	public PortfolioDetailResponse findPortfolioDetail(long userId, long portfolioId) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new IllegalArgumentException(
+				"%d는 존재하지 않는 userId".formatted(userId)));
 
-    @Transactional
-    public PortfolioDetailResponse findPortfolioDetail(long userId,long portfolioId){
-        User user = userRepository.findById(userId)
-            .orElseThrow(() -> new IllegalArgumentException(
-                "%d는 존재하지 않는 userId".formatted(userId)));
+		Portfolio portfolio = portfolioRepository.findById(portfolioId)
+			.orElseThrow(() -> new IllegalArgumentException(
+				"%d는 존재하지 않는 portfolioId".formatted(portfolioId)));
 
-        Portfolio portfolio = portfolioRepository.findById(portfolioId)
-            .orElseThrow(() -> new IllegalArgumentException(
-                "%d는 존재하지 않는 portfolioId".formatted(portfolioId)));
-
-        if(portfolio.getProfile().getId()!=userId){
-            throw new IllegalArgumentException("본인 소유의 포트폴리오가 아닙니다.");
-        }
-        return PortfolioDetailResponse.of(portfolio);
-    }
+		if (portfolio.getProfile().getId() != userId) {
+			throw new IllegalArgumentException("본인 소유의 포트폴리오가 아닙니다.");
+		}
+		return PortfolioDetailResponse.of(portfolio);
+	}
 
 }
