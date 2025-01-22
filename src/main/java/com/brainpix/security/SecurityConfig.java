ef9ae3d9eab045f4a207010f54576a90a8a49edb
redblackblossom <physicsdev1029@gmail.com>
2025-01-22 21:56:33 +0900

    [FEAT] CORS 설정 추가

--- src/main/java/com/brainpix/security/SecurityConfig.java
+++ src/main/java/com/brainpix/security/SecurityConfig.java
@@ -7,6 +7,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -16,10 +19,29 @@ public class SecurityConfig {
 	public SecurityFilterChain publicResourceConfig(HttpSecurity http) throws Exception {
 		http.formLogin(FormLoginConfigurer::disable);
 		http.csrf(AbstractHttpConfigurer::disable);
+		http.cors(
+			cors -> cors.configurationSource(corsConfigurationSource())
+		);
 		http.authorizeHttpRequests(
 			(authorizeRequests)
 				-> authorizeRequests.anyRequest().permitAll()
 		);
 		return http.build();
+	}
+
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.addAllowedOrigin("http://localhost:3000");
+		configuration.addAllowedOrigin("https://www.brainpix.net");
+
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
