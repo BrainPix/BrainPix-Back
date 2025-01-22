8eeb4fcd97d59298a389fe4b78f9a97f30c34c64
redblackblossom <physicsdev1029@gmail.com>
2025-01-22 21:57:50 +0900

    [FEAT] 회원가입 로직 작성

---
+++ src/main/java/com/brainpix/security/service/CompanySignUpService.java
@@ -0,0 +1,36 @@
+package com.brainpix.security.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.brainpix.profile.entity.CompanyProfile;
+import com.brainpix.profile.repository.ProfileRepository;
+import com.brainpix.user.entity.User;
+import com.brainpix.user.repository.UserRepository;
+
+@Service
+public class CompanySignUpService extends SignUpService {
+
+	private final ProfileRepository profileRepository;
+
+	public CompanySignUpService(UserRepository userRepository,
+		PasswordEncoder passwordEncoder,
+		ProfileRepository profileRepository) {
+		super(userRepository, passwordEncoder);
+		this.profileRepository = profileRepository;
+	}
+
+	@Override
+	protected void loginProcess(User user) {
+		CompanyProfile profile = CompanyProfile.builder()
+			.user(user)
+			.specializationList(null)
+			.businessType(null)
+			.businessInformation(null)
+			.homepage(null)
+			.openHomepage(true)
+			.build();
+
+		profileRepository.save(profile);
+	}
+}
