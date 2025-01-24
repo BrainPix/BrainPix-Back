7ae4a846e02ba9ec220264c705fc5d6944c4df4d
qmean <qmean59@gmail.com>
2025-01-24 19:46:58 +0900

    [FEAT] Message 도큐먼트 정의

---
+++ src/main/java/com/brainpix/message/model/Message.java
@@ -0,0 +1,33 @@
+package com.brainpix.message.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.brainpix.jpa.BaseTimeEntity;
+
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@Document(collection = "messages")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Message extends BaseTimeEntity {
+
+	@Id
+	private String id;
+
+	private Long senderId;
+
+	private Long receiverId;
+
+	private Boolean isRead;
+
+	private String title;
+
+	private String content;
+}
