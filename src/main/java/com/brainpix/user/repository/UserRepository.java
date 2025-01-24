2ca5265529de694b3f68d62c0eb2e16aa1bf3313
qmean <qmean59@gmail.com>
2025-01-24 19:45:56 +0900

    [FEAT] UserList 받아오는 로직 추가

--- src/main/java/com/brainpix/user/repository/UserRepository.java
+++ src/main/java/com/brainpix/user/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.brainpix.user.repository;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.brainpix.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	List<User> findAllByIdIn(Set<Long> userIds);
 }
 
 // 테스트 하려고 임의로 만듬
