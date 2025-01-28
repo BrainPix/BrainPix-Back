8ce36567713e48967c86bc37f41f87d0b24654e3
pywoo <kfdsy0103@naver.com>
2025-01-28 22:41:26 +0900

    [REFACT] Query용과 Command용으로 컨트롤러 및 서비스 분리

---
+++ src/main/java/com/brainpix/post/controller/RequestTaskCommandController.java
@@ -0,0 +1,49 @@
+package com.brainpix.post.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brainpix.api.ApiResponse;
+import com.brainpix.post.dto.RequestTaskApiResponseDto;
+import com.brainpix.post.dto.RequestTaskCreateDto;
+import com.brainpix.post.dto.RequestTaskUpdateDto;
+import com.brainpix.post.service.RequestTaskCommandService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/tasks")
+@RequiredArgsConstructor
+public class RequestTaskCommandController {
+
+	private final RequestTaskCommandService requestTaskCommandService;
+
+	@PostMapping
+	public ResponseEntity<ApiResponse<RequestTaskApiResponseDto>> createRequestTask(@RequestParam Long userId,
+		@Valid @RequestBody RequestTaskCreateDto createDto) {
+		Long taskId = requestTaskCommandService.createRequestTask(userId, createDto); // 컨버터행
+		return ResponseEntity.ok(ApiResponse.success(new RequestTaskApiResponseDto("taskId", taskId)));
+	}
+
+	@PutMapping("/{taskId}")
+	public ResponseEntity<ApiResponse<Void>> updateRequestTask(@PathVariable("taskId") Long taskid,
+		@RequestParam Long userId, @Valid @RequestBody RequestTaskUpdateDto updateDto) {
+		requestTaskCommandService.updateRequestTask(taskid, userId, updateDto);
+		return ResponseEntity.ok(ApiResponse.successWithNoData());
+	}
+
+	@DeleteMapping("/{taskId}")
+	public ResponseEntity<ApiResponse<Void>> deleteRequestTask(@PathVariable("taskId") Long taskid,
+		@RequestParam Long userId) {
+		requestTaskCommandService.deleteRequestTask(taskid, userId);
+		return ResponseEntity.ok(ApiResponse.successWithNoData());
+	}
+}
---
+++ src/main/java/com/brainpix/post/controller/RequestTaskQueryController.java
@@ -0,0 +1,56 @@
+package com.brainpix.post.controller;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brainpix.api.ApiResponse;
+import com.brainpix.api.CommonPageResponse;
+import com.brainpix.post.converter.GetPopularRequestTaskListDtoConverter;
+import com.brainpix.post.converter.GetRequestTaskDetailDtoConverter;
+import com.brainpix.post.converter.GetRequestTaskListDtoConverter;
+import com.brainpix.post.dto.GetPopularRequestTaskListDto;
+import com.brainpix.post.dto.GetRequestTaskDetailDto;
+import com.brainpix.post.dto.GetRequestTaskListDto;
+import com.brainpix.post.service.RequestTaskQueryService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/tasks")
+@RequiredArgsConstructor
+public class RequestTaskQueryController {
+
+	private final RequestTaskQueryService requestTaskQueryService;
+
+	@GetMapping
+	public ResponseEntity<ApiResponse<CommonPageResponse<GetRequestTaskListDto.RequestTaskDetail>>> getRequestTaskList(
+		GetRequestTaskListDto.Request request, Pageable pageable) {
+		GetRequestTaskListDto.Parameter parameter = GetRequestTaskListDtoConverter.toParameter(request, pageable);
+		CommonPageResponse<GetRequestTaskListDto.RequestTaskDetail> response = requestTaskQueryService.getRequestTaskList(
+			parameter);
+		return ResponseEntity.ok(ApiResponse.success(response));
+	}
+
+	@GetMapping("/popular")
+	public ResponseEntity<ApiResponse<CommonPageResponse<GetPopularRequestTaskListDto.RequestTaskDetail>>> getPopularRequestTaskList(
+		GetPopularRequestTaskListDto.Request request, Pageable pageable) {
+		GetPopularRequestTaskListDto.Parameter parameter = GetPopularRequestTaskListDtoConverter.toParameter(request,
+			pageable);
+		CommonPageResponse<GetPopularRequestTaskListDto.RequestTaskDetail> response = requestTaskQueryService.getPopularRequestTaskList(
+			parameter);
+		return ResponseEntity.ok(ApiResponse.success(response));
+	}
+
+	@GetMapping("/{taskId}")
+	public ResponseEntity<ApiResponse<GetRequestTaskDetailDto.Response>> getRequestTaskDetail(
+		@PathVariable("taskId") Long taskId
+	) {
+		GetRequestTaskDetailDto.Parameter parameter = GetRequestTaskDetailDtoConverter.toParameter(taskId);
+		GetRequestTaskDetailDto.Response response = requestTaskQueryService.getRequestTaskDetail(parameter);
+		return ResponseEntity.ok(ApiResponse.success(response));
+	}
+}
