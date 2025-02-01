9e0d9e01f24670414b850f962d8e4e8f512ded80
99hyuk <yh99cho1@gmail.com>
2025-02-02 01:42:58 +0900

    [REFACT] PostDto valid설정 수정

--- src/main/java/com/brainpix/post/dto/PostDto.java
+++ src/main/java/com/brainpix/post/dto/PostDto.java
@@ -6,7 +6,6 @@ import com.brainpix.post.entity.PostAuth;
 import com.brainpix.profile.entity.Specialization;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,18 +21,16 @@ public class PostDto {
 	@NotBlank(message = "내용은 비워둘 수 없습니다.")
 	private String content;
 
-	@NotBlank(message = "글 카테고리는 비워둘 수 없습니다.")
+	@NotBlank(message = "분야 카테고리는 비워둘 수 없습니다.")
 	private Specialization specialization;
 
-	@NotNull(message = "프로필 공개 여부는 필수입니다.")
+	@NotNull(message = "프로필 공개 여부는 설정은 필수입니다.")
 	private Boolean openMyProfile;
 
-	@NotEmpty(message = "이미지 리스트는 비워둘 수 없습니다.")
 	private List<String> imageList;
 
-	@NotEmpty(message = "첨부파일 리스트는 비워둘 수 없습니다.")
 	private List<String> attachmentFileList;
 
-	@NotNull(message = "게시글 권한은 필수입니다.")
+	@NotNull(message = "게시글 권한 설정은 필수입니다.")
 	private PostAuth postAuth;
 }
