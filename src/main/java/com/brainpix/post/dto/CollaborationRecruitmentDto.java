9307a7d203cbad2a68011a756748a2f91dec3120
99hyuk <yh99cho1@gmail.com>
2025-02-02 02:05:38 +0900

    [REFACT] 모집 DTO valid 설정

--- src/main/java/com/brainpix/post/dto/CollaborationRecruitmentDto.java
+++ src/main/java/com/brainpix/post/dto/CollaborationRecruitmentDto.java
@@ -3,6 +3,7 @@ package com.brainpix.post.dto;
 import com.brainpix.joining.dto.GatheringDto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,5 +16,6 @@ public class CollaborationRecruitmentDto {
 	@NotBlank(message = "역할은 필수 입력 값입니다.")
 	private String domain;
 
+	@NotNull(message = "모집 인원 수 입력은 필수입니다.")
 	private GatheringDto gatheringDto;
 }
--- src/main/java/com/brainpix/post/dto/RequestTaskRecruitmentDto.java
+++ src/main/java/com/brainpix/post/dto/RequestTaskRecruitmentDto.java
@@ -3,6 +3,7 @@ package com.brainpix.post.dto;
 import com.brainpix.joining.dto.RequestTaskPriceDto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,5 +15,7 @@ public class RequestTaskRecruitmentDto {
 
 	@NotBlank(message = "역할은 필수 입력 값입니다.")
 	private String domain;
+
+	@NotNull(message = "가격 및 인원, 지급 방식 설정은 필수입니다.")
 	private RequestTaskPriceDto requestTaskPriceDto;
 }
