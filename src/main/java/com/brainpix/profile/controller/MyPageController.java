dbb09cfeaeb74e93881e1b6ef2a0de0033832fda
Dongjun <ldj990517@gmail.com>
2025-02-04 05:58:53 +0900

    [REFACT] 인가 + 유저 아이디 userId로 변경

--- src/main/java/com/brainpix/profile/controller/MyPageController.java
+++ src/main/java/com/brainpix/profile/controller/MyPageController.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brainpix.api.ApiResponse;
@@ -22,17 +21,19 @@ public class MyPageController {
 
 	private final MyPageService myPageService;
 
+	@AllUser
 	@Operation(summary = "마이페이지 조회", description = "특정 사용자의 마이페이지 정보를 조회합니다.('내 아이디어','최근소식'은 다른 api로 대체합니다)")
 	@GetMapping
-	public ResponseEntity<ApiResponse<MyPageResponseDto>> getMyPage(@RequestParam Long userId) {
+	public ResponseEntity<ApiResponse<MyPageResponseDto>> getMyPage(@UserId Long userId) {
 		MyPageResponseDto responseDto = myPageService.getMyPage(userId);
 		return ResponseEntity.ok(ApiResponse.success(responseDto));
 	}
 
+	@AllUser
 	@Operation(summary = "'내 아이디어' 조회", description = "특정 사용자가 작성한 아이디어 목록을 페이징 처리하여 조회합니다")
 	@GetMapping("/ideas")
 	public ResponseEntity<ApiResponse<CommonPageResponse<String>>> getMyIdeas(
-		@RequestParam Long userId,
+		@UserId Long userId,
 		Pageable pageable) {
 		return ResponseEntity.ok(
 			ApiResponse.success(CommonPageResponse.of(myPageService.getMyIdeas(userId, pageable))));
