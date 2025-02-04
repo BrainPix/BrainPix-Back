e6afac189b0dc64f3cc48557ffccf8c27871db15
qmean <qmean59@gmail.com>
2025-02-04 22:56:00 +0900

    [REFACTOR] 사용하지 않는 import 제거

--- src/main/java/com/brainpix/alarm/controller/AlarmController.java
+++ src/main/java/com/brainpix/alarm/controller/AlarmController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.brainpix.SwaggerPageable;
 import com.brainpix.alarm.dto.GetAlarmDto;
 import com.brainpix.alarm.dto.GetUnreadAlarmDto;
 import com.brainpix.alarm.service.AlarmService;
@@ -20,11 +17,9 @@ import com.brainpix.security.authorization.AllUser;
 import com.brainpix.security.authorization.UserId;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
