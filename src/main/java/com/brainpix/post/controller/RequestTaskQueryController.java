922b0af00bfb1a8bb5d870cce3145aa3f4da8b20
pywoo <kfdsy0103@naver.com>
2025-02-04 23:22:29 +0900

    [FIX] 동적 쿼리 조회 GET -> POST 변경

--- src/main/java/com/brainpix/post/controller/RequestTaskQueryController.java
+++ src/main/java/com/brainpix/post/controller/RequestTaskQueryController.java
@@ -5,6 +5,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,28 +23,29 @@ import com.brainpix.security.authorization.AllUser;
 import com.brainpix.security.authorization.UserId;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/request-tasks")
 @RequiredArgsConstructor
+@Tag(name = "요청 과제 조회 API", description = "메인 페이지에서 요청과제를 조회하는 API입니다.")
 public class RequestTaskQueryController {
 
 	private final RequestTaskQueryService requestTaskQueryService;
-
-	@AllUser
-	@Operation(summary = "요청 과제 전체 조회", description = "쿼리 파라미터로 요청 과제 타입(OPEN_IDEA, TECH_ZONE)과 검색 조건, page, size를 입력받아 전체 조회합니다.")
-	@GetMapping
+	
+	@Operation(summary = "요청 과제 전체 조회 [POST]", description = "json body로 요청 과제 타입(OPEN_IDEA, TECH_ZONE)과 검색 조건을 입력받고, 페이징을 위한 page, size는 쿼리 파라미터로 입력받아 전체 조회합니다.")
+	@PostMapping("/search")
 	public ResponseEntity<ApiResponse<CommonPageResponse<GetRequestTaskListDto.RequestTaskDetail>>> getRequestTaskList(
-		GetRequestTaskListDto.Request request, @PageableDefault(page = 0, size = 6) Pageable pageable) {
+		@RequestBody GetRequestTaskListDto.Request request, @PageableDefault(page = 0, size = 6) Pageable pageable) {
 		GetRequestTaskListDto.Parameter parameter = GetRequestTaskListDtoConverter.toParameter(request, pageable);
 		CommonPageResponse<GetRequestTaskListDto.RequestTaskDetail> response = requestTaskQueryService.getRequestTaskList(
 			parameter);
 		return ResponseEntity.ok(ApiResponse.success(response));
 	}
 
-	@Operation(summary = "인기 요청 과제 조회", description = "쿼리 파라미터로 요청 과제 타입(OPEN_IDEA, TECH_ZONE)과 page, size를 입력받아 저장순으로 조회합니다.")
-	@GetMapping("/popular")
+	@Operation(summary = "인기 요청 과제 조회 [GET]", description = "쿼리 파라미터로 요청 과제 타입(OPEN_IDEA, TECH_ZONE)과 page, size를 입력받아 저장순으로 조회합니다.")
+	@GetMapping("/search/popular")
 	public ResponseEntity<ApiResponse<CommonPageResponse<GetPopularRequestTaskListDto.RequestTaskDetail>>> getPopularRequestTaskList(
 		GetPopularRequestTaskListDto.Request request, @PageableDefault(page = 0, size = 3) Pageable pageable) {
 		GetPopularRequestTaskListDto.Parameter parameter = GetPopularRequestTaskListDtoConverter.toParameter(request,
@@ -53,7 +56,7 @@ public class RequestTaskQueryController {
 	}
 
 	@AllUser
-	@Operation(summary = "요청 과제 상세 조회", description = "경로 변수로 요청과제 식별자 값을 입력받아 상세 조회합니다.")
+	@Operation(summary = "요청 과제 상세 조회 [GET]", description = "경로 변수로 요청과제 식별자 값을 입력받아 상세 조회합니다.")
 	@GetMapping("/{taskId}")
 	public ResponseEntity<ApiResponse<GetRequestTaskDetailDto.Response>> getRequestTaskDetail(
 		@UserId Long userId,
