8574ec75c73cb06759bc7d87dfdd08a56c19a7d0
redblackblossom <physicsdev1029@gmail.com>
2025-02-04 10:28:03 +0900

    [FEAT]프론트엔드 포트번호 3000에서 5173으로 변경

--- src/main/java/com/brainpix/security/SecurityConfig.java
+++ src/main/java/com/brainpix/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 
-		configuration.addAllowedOrigin("http://localhost:3000");
+		configuration.addAllowedOrigin("http://localhost:5173");
 		configuration.addAllowedOrigin("https://www.brainpix.net");
 
 		configuration.addAllowedHeader("*");
