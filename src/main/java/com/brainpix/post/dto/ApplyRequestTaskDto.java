cbea8ba49c0fd9648c71dad0ceccd927056ff43b
pywoo <kfdsy0103@naver.com>
2025-02-05 00:15:30 +0900

    [REFACT] 인증 및 인가, 스웨거 설명 추가

--- src/main/java/com/brainpix/post/dto/ApplyRequestTaskDto.java
+++ src/main/java/com/brainpix/post/dto/ApplyRequestTaskDto.java
@@ -4,13 +4,11 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 public class ApplyRequestTaskDto {
 
 	@NoArgsConstructor
 	@Getter
-	@Setter
 	public static class Request {
 		@NotNull(message = "지원 분야는 필수입니다.")
 		Long requestRecruitmentId;    // 지원 분야 식별자
