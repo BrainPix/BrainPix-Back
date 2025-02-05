e72622423b7da6d110e2c5686257b316a93a8278
Dongjun <ldj990517@gmail.com>
2025-02-06 07:26:40 +0900

    [REFACT] @Scheme 추가

--- src/main/java/com/brainpix/profile/dto/request/PortfolioRequest.java
+++ src/main/java/com/brainpix/profile/dto/request/PortfolioRequest.java
@@ -4,11 +4,14 @@ import java.time.YearMonth;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.brainpix.profile.entity.Portfolio;
 import com.brainpix.profile.entity.Profile;
 import com.brainpix.profile.entity.Specialization;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record PortfolioRequest(
@@ -16,9 +19,13 @@ public record PortfolioRequest(
 	String title,
 	List<SpecializationRequest> specializations,
 	@NotBlank(message = "시작 날짜를 입력해주세요.")
+	@Schema(type = "string", example = "yyyy-MM")
+	@DateTimeFormat(pattern = "yyyy-MM")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM")
 	YearMonth startDate,
 	@NotBlank(message = "종료 날짜를 입력해주세요.")
+	@Schema(type = "string", example = "yyyy-MM")
+	@DateTimeFormat(pattern = "yyyy-MM")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM")
 	YearMonth endDate,
 	@NotBlank(message = "포트폴리오 내용을 입력해주세요.")
--- src/main/java/com/brainpix/profile/dto/response/PortfolioDetailResponse.java
+++ src/main/java/com/brainpix/profile/dto/response/PortfolioDetailResponse.java
@@ -3,17 +3,25 @@ package com.brainpix.profile.dto.response;
 import java.time.YearMonth;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.brainpix.profile.entity.Portfolio;
 import com.brainpix.profile.entity.Specialization;
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record PortfolioDetailResponse(
 	long id,
 	String title,
 	List<Specialization> specializations,
+	@Schema(type = "string", example = "yyyy-MM")
+	@DateTimeFormat(pattern = "yyyy-MM")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM")
 	YearMonth startDate,
 
+	@Schema(type = "string", example = "yyyy-MM")
+	@DateTimeFormat(pattern = "yyyy-MM")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM")
 	YearMonth endDate,
 	String content,
