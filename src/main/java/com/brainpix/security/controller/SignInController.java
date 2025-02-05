77651726e2249c2f1443c9e7c1308b9a72b697cc
조경호 <155342235+redblackblossom@users.noreply.github.com>
2025-02-05 14:03:23 +0900

    [FIX] 로그인시 토큰을 헤더가 아닌 바디로 전달 (#141)
    [FIX] 로그인시 토큰을 헤더가 아닌 바디로 전달 (#141)

--- src/main/java/com/brainpix/security/controller/SignInController.java
+++ src/main/java/com/brainpix/security/controller/SignInController.java
@@ -8,15 +8,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brainpix.api.ApiResponse;
 import com.brainpix.security.converter.SignInConverter;
 import com.brainpix.security.dto.request.SignInRequest;
+import com.brainpix.security.dto.response.SignInResponse;
 import com.brainpix.security.tokenManger.TokenManager;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-@Tag(name = "로그인 API", description = "로그인 API입니다. 헤더에 Authorization : Bearer {JWT토큰} 형태로 응답합니다")
+@Tag(name = "로그인 API", description = "로그인 API입니다.")
 @RestController
 @RequestMapping("/users/login")
 @RequiredArgsConstructor
@@ -26,12 +28,10 @@ public class SignInController {
 
 	@Operation(summary = "로그인 API", description = "권한에 관계없이 1개의 API로 통합되어 있습니다.")
 	@PostMapping
-	public ResponseEntity<Void> singIn(@RequestBody SignInRequest signInRequest) {
+	public ResponseEntity<ApiResponse<SignInResponse>> singIn(@RequestBody SignInRequest signInRequest) {
 		Authentication authentication = authenticationManager.authenticate(
 			SignInConverter.toAuthenticationToken(signInRequest));
 		String jwt = tokenManager.writeToken(authentication);
-		return ResponseEntity.ok()
-			.header("Authorization", "Bearer " + jwt)
-			.body(null);
+		return ResponseEntity.ok(ApiResponse.success(new SignInResponse("Bearer " + jwt)));
 	}
 }
