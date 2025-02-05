cd3b71d5b259fc2c91cfc91a7aacc6b0a5de546f
99hyuk <yh99cho1@gmail.com>
2025-02-05 22:08:48 +0900

    [REFACT] 불필요한 모듈 삭제

--- src/main/java/com/brainpix/post/dto/RequestTaskUpdateDto.java
+++ src/main/java/com/brainpix/post/dto/RequestTaskUpdateDto.java
@@ -1,11 +1,8 @@
 package com.brainpix.post.dto;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-import com.brainpix.post.entity.PostAuth;
 import com.brainpix.post.entity.request_task.RequestTaskType;
-import com.brainpix.profile.entity.Specialization;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
