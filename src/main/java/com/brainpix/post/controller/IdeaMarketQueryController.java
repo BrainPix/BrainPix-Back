3ff0776c56b0cd0fe3c3d4e1132cd6be70d7d69b
redblackblossom <physicsdev1029@gmail.com>
2025-02-06 16:30:51 +0900

    [REFACT] 아이디어 마켓 커맨드, 쿼리 분리

--- src/main/java/com/brainpix/post/controller/IdeaMarketQueryController.java
+++ src/main/java/com/brainpix/post/controller/IdeaMarketQueryController.java
@@ -4,12 +4,10 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +21,6 @@ import com.brainpix.post.converter.GetPopularIdeaListDtoConverter;
 import com.brainpix.post.dto.GetIdeaDetailDto;
 import com.brainpix.post.dto.GetIdeaListDto;
 import com.brainpix.post.dto.GetPopularIdeaListDto;
-import com.brainpix.post.dto.IdeaMarketCreateDto;
-import com.brainpix.post.dto.IdeaMarketUpdateDto;
-import com.brainpix.post.dto.PostApiResponseDto;
 import com.brainpix.post.service.IdeaMarketService;
 import com.brainpix.security.authorization.AllUser;
 import com.brainpix.security.authorization.UserId;
@@ -40,37 +35,10 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/idea-markets")
 @RequiredArgsConstructor
 @Slf4j
-@Tag(name = "아이디어 마켓 API", description = "아이디어 마켓 관련 API")
-public class IdeaMarketController {
+@Tag(name = "아이디어 마켓 검색 API", description = "아이디어 마켓 관련 API")
+public class IdeaMarketQueryController {
 
 	private final IdeaMarketService ideaMarketService;
-
-	@AllUser
-	@Operation(summary = "아이디어 마켓 글 생성", description = "아이디어 마켓 게시글을 생성합니다.")
-	@PostMapping
-	public ResponseEntity<ApiResponse<PostApiResponseDto>> createIdeaMarket(@UserId Long userId,
-		@Valid @RequestBody IdeaMarketCreateDto createDto) {
-		Long ideaId = ideaMarketService.createIdeaMarket(userId, createDto);
-		return ResponseEntity.ok(ApiResponse.success(new PostApiResponseDto("ideaId", ideaId)));
-	}
-
-	@AllUser
-	@Operation(summary = "아이디어 마켓 글 수정", description = "아이디어 마켓 게시글을 수정합니다.")
-	@PutMapping("/{ideaId}")
-	public ResponseEntity<ApiResponse<Void>> updateIdeaMarket(@PathVariable("ideaId") Long ideaId,
-		@UserId Long userId, @Valid @RequestBody IdeaMarketUpdateDto updateDto) {
-		ideaMarketService.updateIdeaMarket(ideaId, userId, updateDto);
-		return ResponseEntity.ok(ApiResponse.successWithNoData());
-	}
-
-	@AllUser
-	@Operation(summary = "아이디어 마켓 글 삭제", description = "아이디어 마켓 게시글을 삭제합니다.")
-	@DeleteMapping("/{ideaId}")
-	public ResponseEntity<ApiResponse<Void>> deleteIdeaMarket(@PathVariable("ideaId") Long ideaId,
-		@UserId Long userId) {
-		ideaMarketService.deleteIdeaMarket(ideaId, userId);
-		return ResponseEntity.ok(ApiResponse.successWithNoData());
-	}
 
 	@SwaggerPageable
 	@Operation(summary = "아이디어 전체 조회 [POST]", description =
