8de031abc811974d0ee55c1aee152770d1f0f159
pywoo <81312085+kfdsy0103@users.noreply.github.com>
2025-02-06 22:28:45 +0900

    [FEAT] 조회수 증가 로직 구현 (#152) (#155)
    * [FIX] @Valid 누락 추가
    
    * [FEAT] 조회수 증가 로직 추가
    
    * [FEAT] 조회수 증가 로직 추가로 인한 readOnly=true 제거

--- src/main/java/com/brainpix/post/repository/PostRepository.java
+++ src/main/java/com/brainpix/post/repository/PostRepository.java
@@ -3,6 +3,9 @@ package com.brainpix.post.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.brainpix.post.entity.Post;
 import com.brainpix.user.entity.User;
@@ -10,4 +13,8 @@ import com.brainpix.user.entity.User;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 	Page<Post> findByWriter(User writer, Pageable pageable);
+
+	@Modifying
+	@Query("update Post p set p.viewCount = p.viewCount + 1 where p.id = :id")
+	Integer increaseViewCount(@Param("id") Long postId);
 }
