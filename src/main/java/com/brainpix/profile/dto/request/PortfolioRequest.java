7e9021e272dd11e9bf4fed77bb7a0a6c83a0113b
Dongjun <ldj990517@gmail.com>
2025-02-06 12:37:03 +0900

    [FIX] Specialization 요청, 응답 형태 통일위한 수정

--- src/main/java/com/brainpix/profile/dto/request/PortfolioRequest.java
+++ src/main/java/com/brainpix/profile/dto/request/PortfolioRequest.java
@@ -17,7 +17,7 @@ import jakarta.validation.constraints.NotBlank;
 public record PortfolioRequest(
 	@NotBlank(message = "포트폴리오 제목을 입력해주세요.")
 	String title,
-	List<SpecializationRequest> specializations,
+	List<Specialization> specializations,
 	@NotBlank(message = "시작 날짜를 입력해주세요.")
 	@Schema(type = "string", example = "yyyy-MM")
 	@DateTimeFormat(pattern = "yyyy-MM")
@@ -37,16 +37,11 @@ public record PortfolioRequest(
 	 * DTO -> Entity 변환 (생성 시 사용)
 	 */
 	public Portfolio toEntity(Profile profile) {
-		List<Specialization> specs = Optional.ofNullable(specializations)
-			.orElse(List.of())
-			.stream()
-			.map(SpecializationRequest::toDomain)
-			.toList();
 
 		return Portfolio.create(
 			profile,
 			title,
-			specs,
+			specializations,
 			startDate,
 			endDate,
 			content,
@@ -58,15 +53,10 @@ public record PortfolioRequest(
 	 * 엔티티 수정에 반영할 메서드 (update)
 	 */
 	public void applyTo(Portfolio portfolio) {
-		List<Specialization> specs = Optional.ofNullable(specializations)
-			.orElse(List.of())
-			.stream()
-			.map(SpecializationRequest::toDomain)
-			.toList();
 
 		portfolio.update(
 			Optional.ofNullable(title).orElse(portfolio.getTitle()),  // null이면 기존 값 유지
-			specs,
+			Optional.ofNullable(specializations).orElse(portfolio.getSpecializationList()),  // null이면 기존 값 유지
 			Optional.ofNullable(startDate).orElse(portfolio.getStartDate()),
 			Optional.ofNullable(endDate).orElse(portfolio.getEndDate()),
 			Optional.ofNullable(content).orElse(portfolio.getContent()),
